be86cde46c93a0c7dd9c13593bedeab85aa91ed3
Musa Wong <musaiw@outlook.com>
2025-03-20 17:46:03 +0300

    wrap with safearea

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,17 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import useColours from "./colours";
 
 export default function RootLayout() {
   const colours = useColours();
   return (
-    <Stack screenOptions={{ statusBarBackgroundColor: colours.background }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="history" options={{ headerShown: false }} />
-    </Stack>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: colours.background }}>
+        <Stack screenOptions={{ statusBarBackgroundColor: colours.background }}>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+          <Stack.Screen name="history" options={{ headerShown: false }} />
+        </Stack>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
